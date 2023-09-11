362b0ffd6325e343112c4a13ffbd48a97a5aa5fa
Jag <jag3dagster@gmail.com>
2023-09-11 03:13:09 -0700

    feat: Update to 1.6 compat

--- src/AllowBeachSprinklers/Helpers/TileEqualityComparer.cs
+++ src/AllowBeachSprinklers/Helpers/TileEqualityComparer.cs
@@ -8,8 +8,8 @@ namespace AllowBeachSprinklers.Helpers
 		public bool Equals(Tile x, Tile y)
 		{
 			if (ReferenceEquals(x, y)) return true;
-			if (ReferenceEquals(x, null)) return false;
-			if (ReferenceEquals(y, null)) return false;
+			if (x is null) return false;
+			if (y is null) return false;
 			if (x.GetType() != y.GetType()) return false;
 
 			return Equals(x.TileSheet, y.TileSheet) && x.TileIndex == y.TileIndex;
@@ -20,10 +20,12 @@ namespace AllowBeachSprinklers.Helpers
 			unchecked
 			{
 				var hashCode = (obj.Layer != null ? obj.Layer.GetHashCode() : 0);
+
 				hashCode = (hashCode * 397) ^ (int) obj.BlendMode;
 				hashCode = (hashCode * 397) ^ (obj.TileSheet != null ? obj.TileSheet.GetHashCode() : 0);
 				hashCode = (hashCode * 397) ^ obj.TileIndex;
 				hashCode = (hashCode * 397) ^ (obj.TileIndexProperties != null ? obj.TileIndexProperties.GetHashCode() : 0);
+
 				return hashCode;
 			}
 		}
---
+++ src/AllowBeachSprinklers/ModEntry.cs
@@ -0,0 +1,42 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using xTile.Tiles;
+
+using AllowBeachSprinklers.Helpers;
+using System.Linq;
+
+namespace AllowBeachSprinklers
+{
+	public class ModEntry : Mod
+	{
+		private const int BEACH_FARM = 6;
+		private const string NO_SPRINKLERS_KEY = "NoSprinklers";
+		private const string SAND_LAYER_NAME = "Back";
+
+		public override void Entry(IModHelper helper)
+		{
+			helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
+		}
+
+		private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
+		{
+			if (Game1.whichFarm != BEACH_FARM) return;
+
+			var farm = Game1.getFarm();
+			var tileList = farm.Map.GetLayer(SAND_LAYER_NAME).Tiles.Array.Cast<Tile>().ToList();
+
+			var noSprinklerTiles = tileList
+				.Where(tile => tile?.TileIndexProperties != null && tile.TileIndexProperties.ContainsKey(NO_SPRINKLERS_KEY))
+				.Distinct(new TileEqualityComparer())
+				.OrderBy(tile => tile.TileIndex);
+
+			foreach (var tile in noSprinklerTiles)
+			{
+				tile.Properties[NO_SPRINKLERS_KEY] = "false";
+			}
+
+			Monitor.Log("Sprinklers can now be placed in sand on your farm.", LogLevel.Info);
+		}
+	}
+}
