c2f19b44291eea01ab357c878527940819f9c721
Jag <jag3dagster@gmail.com>
2023-09-22 23:10:13 -0700

    chore: Minor formatting changes

--- src/AllowBeachSprinklers/ModEntry.cs
+++ src/AllowBeachSprinklers/ModEntry.cs
@@ -1,18 +1,18 @@
-﻿using StardewModdingAPI;
+﻿using System.Linq;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using xTile.Tiles;
-
 using AllowBeachSprinklers.Helpers;
-using System.Linq;
+
 
 namespace AllowBeachSprinklers
 {
 	public class ModEntry : Mod
 	{
-		private const int BEACH_FARM = 6;
-		private const string NO_SPRINKLERS_KEY = "NoSprinklers";
-		private const string SAND_LAYER_NAME = "Back";
+		private const int BeachFarm = 6;
+		private const string NoSprinklersKey = "NoSprinklers";
+		private const string SandLayerName = "Back";
 
 		public override void Entry(IModHelper helper)
 		{
@@ -21,19 +21,19 @@ namespace AllowBeachSprinklers
 
 		private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
 		{
-			if (Game1.whichFarm != BEACH_FARM) return;
+			if (Game1.whichFarm != BeachFarm) return;
 
 			var farm = Game1.getFarm();
-			var tileList = farm.Map.GetLayer(SAND_LAYER_NAME).Tiles.Array.Cast<Tile>().ToList();
+			var tileList = farm.Map.GetLayer(SandLayerName).Tiles.Array.Cast<Tile>().ToList();
 
 			var noSprinklerTiles = tileList
-				.Where(tile => tile?.TileIndexProperties != null && tile.TileIndexProperties.ContainsKey(NO_SPRINKLERS_KEY))
+				.Where(tile => tile?.TileIndexProperties != null && tile.TileIndexProperties.ContainsKey(NoSprinklersKey))
 				.Distinct(new TileEqualityComparer())
 				.OrderBy(tile => tile.TileIndex);
 
 			foreach (var tile in noSprinklerTiles)
 			{
-				tile.TileIndexProperties[NO_SPRINKLERS_KEY] = false;
+				tile.TileIndexProperties[NoSprinklersKey] = false;
 			}
 
 			Monitor.Log("Sprinklers can now be placed in sand on your farm.", LogLevel.Info);
